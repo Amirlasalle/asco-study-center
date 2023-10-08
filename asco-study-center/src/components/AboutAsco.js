00d5d8c21a3441450ecd3dc4bad6c0d4844665a2
アミール　モハメド <mariemosalca@Amirs-MacBook.fios-router.home>
2023-10-08 19:13:18 -0400

    completed asco timeline.

--- asco-study-center/src/components/AboutAsco.js
+++ asco-study-center/src/components/AboutAsco.js
@@ -1,34 +1,61 @@
+import { useState } from 'react';
+import "../App.css"
+import ascotimelineData from "./ascotimeline.json"
 import Col from 'react-bootstrap/Col';
 import Container from 'react-bootstrap/Container';
-import Image from 'react-bootstrap/Image';
 import Row from 'react-bootstrap/Row';
-import "../App.css"
+// import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
 
 const AboutAsco = () => {
+  const [ascotimeline] = useState(ascotimelineData)
+  console.log(ascotimeline)
   return (<>
 
     <Container id="aboutasco" className=' mt-4 mb-auto' >
       <h2 className="text-center section-title">
-  About ASCO
-  <span className="smaller" style={{ display: 'inline-block', verticalAlign: 'top' }}>
-    Power<br /> technologies
-  </span>
-</h2>
+        About ASCO
+        <span className="smaller" style={{ display: 'inline-block', verticalAlign: 'top' }}>
+          Power<br /> technologies
+        </span>
+      </h2>
 
       <Row>
 
         <Col xs={9} md={5}>
           <p className='abouttext'>
-          
-          At ASCO, we have a long history of innovation. In fact, it's part of our DNA. Since 1888, ASCO has prided itself on providing cutting edge technology and never compromising on quality.
-          
+
+            At ASCO, we have a long history of innovation. In fact, it's part of our DNA. Since 1888, ASCO has prided itself on providing cutting edge technology and never compromising on quality.
+
           </p>
 
         </Col>
-       
-        <Col>
-        <Image className='asco-logo' src={process.env.PUBLIC_URL + '/assets/nasa.jpg'} fluid /></Col>
 
+
+      </Row>
+
+      <Row>
+        <h3 className="text-left timeline-title">Discover ASCO's Timeline</h3>
+        <div id="ascotimeline" className="d-flex flex-wrap justify-content-around ">
+          {ascotimeline.map((project, key) =>
+
+            <Card key={key} className="m-2 p-2" style={{ width: '18rem' }}>
+              <Card.Body>
+                <Card.Title>{project.year}</Card.Title>
+                <Card.Text>{project.brief}
+                </Card.Text>
+                <Card.Text>{project.brief2}
+                </Card.Text>
+                <Card.Text>{project.brief3}
+                </Card.Text>
+
+                {/* <Card.Link href="#">Card Link</Card.Link>
+                  <Card.Link href="#">Another Link</Card.Link> */}
+              </Card.Body>
+            </Card>
+
+          )}
+        </div>
       </Row>
     </Container>
 
