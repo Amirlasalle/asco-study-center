53222b1b92c58494ec02ef966af0760a86cf78a4
アミール　モハメド <mariemosalca@Amirs-MacBook.fios-router.home>
2023-10-08 22:00:13 -0400

    added youtube iframe to about ASCO page.

--- asco-study-center/src/components/AboutAsco.js
+++ asco-study-center/src/components/AboutAsco.js
@@ -5,7 +5,11 @@ import Col from 'react-bootstrap/Col';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 // import Button from 'react-bootstrap/Button';
+import Image from 'react-bootstrap/Image';
 import Card from 'react-bootstrap/Card';
+import Carousel from 'react-bootstrap/Carousel';
+
+
 
 const AboutAsco = () => {
   const [ascotimeline] = useState(ascotimelineData)
@@ -22,24 +26,58 @@ const AboutAsco = () => {
 
       <Row>
 
-        <Col xs={9} md={5}>
-          <p className='abouttext'>
+        <Col >
+          <Carousel slide={false}><Carousel.Item>
+            <Image src={process.env.PUBLIC_URL + "/assets/nasa.jpg"} className="img-fluid d-flex flex-wrap justify-content-around about-slide-image" />
+            <Carousel.Caption className='position-relativeslide-title about-outlined-text'>
+              <h5 className='position-relativeslide-title about-outlined-text'>
+                At ASCO Power Technologies, we have a long history of innovation. We are on a constant drive to bring new technologies and new products to our customers to help them become more productive.</h5><br></br>
+            </Carousel.Caption>
+          </Carousel.Item>
+            <Carousel.Item>
+              <Image src={process.env.PUBLIC_URL + "/assets/atslinedup.jpeg"} className="img-fluid d-flex flex-wrap justify-content-around about-slide-image" />
+              <Carousel.Caption className='position-relativeslide-title about-outlined-cap'>
+                <h5 className='position-relativeslide-title about-outlined-text'>
+                  At ASCO, we have a long history of innovation. In fact, it's part of our DNA. Since 1888, ASCO has prided itself on providing cutting edge technology and never compromising on quality.</h5><br></br>
+              </Carousel.Caption>
+            </Carousel.Item>
+          </Carousel>
 
-            At ASCO, we have a long history of innovation. In fact, it's part of our DNA. Since 1888, ASCO has prided itself on providing cutting edge technology and never compromising on quality.
 
-          </p>
 
         </Col>
 
 
       </Row>
 
-      <Row>
-        <h3 className="text-left timeline-title">Discover ASCO's Timeline</h3>
+      <Row className='mt-3 custom-color1'>
+
+        <Col >
+          <Carousel slide={false}>   <Carousel.Item>
+            <div className="video-container">
+              <iframe width="560" height="315" src="https://www.youtube.com/embed/RqFZ7Xeumuo?si=L9utuwueByryT-0T" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+            </div>
+          </Carousel.Item>
+            <Carousel.Item>
+              <div className="video-container">
+                <iframe width="560" height="315" src="https://www.youtube.com/embed/0LAsZU44VXc?si=VI9_w0WcUjV9ebsL" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+              </div>
+            </Carousel.Item>
+          </Carousel>
+
+
+
+        </Col>
+
+
+      </Row>
+
+      <Row className='mt-4 custom-color'>
+        <h3 className="mt-3 text-left timeline-title">Discover ASCO's Timeline</h3>
         <div id="ascotimeline" className="d-flex flex-wrap justify-content-around ">
           {ascotimeline.map((project, key) =>
 
-            <Card key={key} className="m-2 p-2" style={{ width: '18rem' }}>
+            <Card key={key} className="m-2 p-2 shadow-lg" style={{ width: '18rem' }}>
               <Card.Body>
                 <Card.Title>{project.year}</Card.Title>
                 <Card.Text>{project.brief}
--- asco-study-center/src/pages/PageContainer.js
+++ asco-study-center/src/pages/PageContainer.js
@@ -23,8 +23,8 @@ const PageContainer = () => {
             <Carousel.Item>
               <Image src={process.env.PUBLIC_URL + "/assets/atslinedup.jpeg"} className="img-fluid d-flex flex-wrap justify-content-around slide-image" />
               <Carousel.Caption>
-                <h2 className='position-relativeslide-title outlined-text'>ASCO Power Technologies ensures uninterrupted operational performance.</h2><br></br>
-                <Button variant="info" size="md" ><a href='https://www.youtube.com/watch?v=3zNO-2RBqKY' target="_blank" rel="noreferrer" className="btn1" >Learn More</a></Button>
+                <h4 className='position-relativeslide-title outlined-text'>ASCO Power Technologies ensures uninterrupted operational performance.</h4><br></br>
+                <Button variant="info" size="md" className="carousel-btn"><a href='https://www.youtube.com/watch?v=3zNO-2RBqKY' target="_blank" rel="noreferrer" className="btn1" >Learn More</a></Button>
               </Carousel.Caption>
             </Carousel.Item>
             <Carousel.Item>
@@ -43,9 +43,9 @@ const PageContainer = () => {
             </Carousel.Item>
             <Carousel.Item>
               <Image src={process.env.PUBLIC_URL + "/assets/nts.jpeg"} className="img-fluid d-flex flex-wrap justify-content-around slide-image" />
-              <Carousel.Caption>
-                <h2 className='position-relativeslide-title outlined-text'>ASCO Power Technologies and the NTS{'(Non-Automatic Transfer Switch)'} and why it might be a great alternative for a Manual Transfer Switch.</h2><br></br>
-                <Button variant="info" size="md" ><a href='https://www.youtube.com/watch?v=lbs_BC8cQwo' target="_blank" rel="noreferrer" className="btn1" >Learn More</a></Button>
+              <Carousel.Caption className=' mt-0 mb-auto  justify-content-center position-relativeslide-title outlined-text'>
+                <h2 className='d-flex justify-content-space-around position-relativeslide-title outlined-text'>ASCO Power Technologies and the NTS{'(Non-Automatic Transfer Switch)'} and why it might be a great alternative for a Manual Transfer Switch.</h2>
+                <Button variant="info" size="md" className="mt-0"><a href='https://www.youtube.com/watch?v=lbs_BC8cQwo' target="_blank" rel="noreferrer" className="btn1" >Learn More</a></Button>
               </Carousel.Caption>
             </Carousel.Item>
             <Carousel.Item>
