fb48c920b8cff29c0cf355f70178d9bf4a8aa2ca
アミール　モハメド <mariemosalca@Amirs-MacBook.fios-router.home>
2023-10-08 23:43:14 -0400

    added products to Our Products page.

--- asco-study-center/src/components/Products.js
+++ asco-study-center/src/components/Products.js
@@ -14,25 +14,19 @@ const Products = () => {
         <Container id="productinfo" className="d-flex flex-wrap justify-content-around ">
             {productinfo.map((productinfo, key) =>
 
-                <Card key={key} className="m-2 p-2" style={{ width: '18rem' }}>
+                <Card key={key} className="m-2 p-2 shadow-lg" style={{ width: '18rem' }}>
                     <Card.Img variant="top" src={process.env.PUBLIC_URL + productinfo.screenshot} />
                     <Card.Body>
-                        <Card.Title>{productinfo.appname}</Card.Title>
-                        <Card.Text>{productinfo.breif}
+                        <Card.Title className="card-titles">{productinfo.productname}</Card.Title>
+                        <Card.Subtitle className="mb-2  card-subt">{productinfo.subtitle}</Card.Subtitle>
+                        <Card.Subtitle className="mb-3 text-muted  card-subt2">{productinfo.subtitle2}</Card.Subtitle>
+                        <Card.Text>{productinfo.brief}
                         </Card.Text>
                         <style type="text/css">
-                            {`
-    .btn-flat {
-      background-color: #3dcd58;
-      color: black;
-    }
-    .btn:hover{
-    background-color: yellow;
-    }
-    `}
                         </style>
-                        <Button variant="flat" size="md" ><a href={productinfo.github} target="_blank" rel="noreferrer" className="btn1" >Github <br></br>Repository</a></Button>&nbsp;&nbsp;&nbsp;
-                        <Button variant="info"  ><a href={productinfo.deployment} target="_blank" rel="noreferrer" className="btn1"  >Live <br></br>Deployment</a></Button>
+                        <Button variant="info" size="md" className="mb-1 mt-auto" ><a href={productinfo.product} target="_blank" rel="noreferrer" className="btn1" >More information <span><i class="fa fa-external-link" aria-hidden="true"></i></span></a></Button>
+                        {/* &nbsp;&nbsp;&nbsp;
+                        <Button variant="info"  ><a href={productinfo.deployment} target="_blank" rel="noreferrer" className="btn1"  >Contact <br></br>sales</a></Button> */}
                     </Card.Body>
                 </Card>
             )}
