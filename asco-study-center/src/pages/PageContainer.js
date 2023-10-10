37f54bddeaf701583e515b8843455d69336fd783
アミール　モハメド <mariemosalca@Amirs-MacBook.fios-router.home>
2023-10-09 21:51:07 -0400

    added dim effect on images in the pageContainer.

--- asco-study-center/src/pages/PageContainer.js
+++ asco-study-center/src/pages/PageContainer.js
@@ -34,8 +34,8 @@ const PageContainer = () => {
           <Carousel slide={false} nextIcon={<FontAwesomeIcon icon={faChevronRight} />} style={nextIconStyle}
             prevIcon={<FontAwesomeIcon icon={faChevronLeft} />}>
             <Carousel.Item>
-              <Image src={process.env.PUBLIC_URL + "/assets/atslinedup.jpeg"} className="img-fluid d-flex flex-wrap justify-content-around slide-image" />
-              
+              <Image src={process.env.PUBLIC_URL + "/assets/gentransferswitch.jpeg"} className="img-fluid d-flex flex-wrap justify-content-around slide-image" />
+
               <Carousel.Caption className='mt-0 mb-auto d-flex flex-column justify-content-center position-relativeslide-title outlined-text text-center'>
                 <h className='position-relativeslide-title outlined-text'>ASCO Power Technologies ensures uninterrupted operational performance</h>
                 <Button variant="info" size="md" className="carousel-btn btn-block mx-auto" style={{ maxWidth: '200px' }}>
@@ -44,7 +44,7 @@ const PageContainer = () => {
               </Carousel.Caption>
             </Carousel.Item>
             <Carousel.Item>
-              <Image src={process.env.PUBLIC_URL + "/assets/7000bypass.jpeg"} className="img-fluid d-flex flex-wrap justify-content-around slide-image" />
+              <Image src={process.env.PUBLIC_URL + "/assets/asco7000bypassiso.jpeg"} className="img-fluid d-flex flex-wrap justify-content-around slide-image" />
 
               <Carousel.Caption className='mt-0 mb-auto d-flex flex-column justify-content-center position-relativeslide-title outlined-text text-center'>
                 <h className='position-relativeslide-title outlined-text'>Keep seeking knowledge!</h>
@@ -60,11 +60,11 @@ const PageContainer = () => {
               </Carousel.Caption>
             </Carousel.Item>
             <Carousel.Item>
-              <Image src={process.env.PUBLIC_URL + "/assets/nts.jpeg"} className="img-fluid d-flex flex-wrap justify-content-around slide-image" />
+              <Image src={process.env.PUBLIC_URL + "/assets/powerpanels.jpeg"} className="img-fluid d-flex flex-wrap justify-content-around slide-image" />
 
               <Carousel.Caption className='mt-0 mb-auto d-flex flex-column justify-content-center position-relativeslide-title outlined-text text-center'>
 
-                <h className='position-relativeslide-title outlined-text'>ASCO offers the Non-Automatic Transfer Switch as a Manual Switch alternative</h>
+                <h className='position-relativeslide-title outlined-text'>ASCO Quick Connects ensure a reliable way to connect generators to infrastructures.</h>
                 <Button variant="info" size="md" className="carousel-btn btn-block mx-auto" style={{ maxWidth: '200px' }}><a href='https://www.youtube.com/watch?v=lbs_BC8cQwo' target="_blank" rel="noreferrer" className="btn1" >Learn More <span><i class="fa fa-external-link" aria-hidden="true"></i></span></a></Button>
               </Carousel.Caption>
             </Carousel.Item>
@@ -78,7 +78,7 @@ const PageContainer = () => {
               </Carousel.Caption>
             </Carousel.Item>
             <Carousel.Item>
-              <Image src={process.env.PUBLIC_URL + "/assets/ascodatacenter.jpeg"} className="img-fluid d-flex flex-wrap justify-content-around slide-image" />
+              <Image src={process.env.PUBLIC_URL + "/assets/datacenterfiber.png"} className="img-fluid d-flex flex-wrap justify-content-around slide-image" />
 
               <Carousel.Caption className='mt-0 mb-auto d-flex flex-column justify-content-center position-relativeslide-title outlined-text text-center'>
                 <h className='position-relativeslide-title outlined-text'>Data Centers nationwide trust ASCO for unmatched reliability</h>
@@ -97,51 +97,55 @@ const PageContainer = () => {
 
 
 
- <Row className='mt-4 custom-color'>
-        <h3 className="mt-3 mb-3 bg-white text-left timeline-title">Welcome to the ASCO SDP Study Center! This is the starting point of our knowledge journey. Are you prepared?</h3>
-        <div id="homepageCards" className="d-flex flex-wrap justify-content-around ">
-          {homepagecards.map((project, key) =>
+          <Row className='mt-4 custom-color'>
+            <h3 className="mt-3 mb-3 bg-white text-left timeline-title" style={{ borderColor: "#fff", backgroundColor: "#3dcd58", boxShadow: '0 0 30px rgba(0, 0, 0, 0.5)' }}><br></br><br></br>Welcome to the ASCO SDP Study Center! This is the starting point of your knowledge journey. Are you prepared? <Button variant="info" size="md" className="carousel-btn btn-block mx-auto m-2" style={{ maxWidth: '200px' }}><a href='https://www.youtube.com/watch?v=wdvqnmcBFpo&t=1s' target="_blank" rel="noreferrer" className="btn1" >Start Learning <span><i class="fa fa-external-link" aria-hidden="true"></i></span></a></Button><br></br><br></br><br></br></h3>
+            <div id="homepageCards" className="d-flex flex-wrap justify-content-around ">
+              {homepagecards.map((project, key) =>
 
-          <Card key={key} variant="secondary" className="m-2 p-2 " style={{ width: '18rem', borderColor: "#fff", backgroundColor: "#3dcd58", boxShadow: '0 0 30px rgba(0, 0, 0, 0.5)' }}>
-              <Card.Body>
-                <Card.Title className="text-light">{project.title}</Card.Title><iframe width="240rem" height="auto" src={project.contentUrl} title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
-                <Card.Text className="text-white">{project.description}
-                </Card.Text>
-                <Card.Text>{project.brief2}
-                </Card.Text>
-                <Card.Text>{project.brief3}
-                </Card.Text>
-                <Button variant="secondary" size="md" className="mb-1 mt-auto" ><a href={project.contentUrlButton} target="_blank" rel="noreferrer" className="btn1" >More information <span><i class="fa fa-external-link" aria-hidden="true"></i></span></a></Button>
-                {/* <Card.Link href="#">Card Link</Card.Link>
+                <Card key={key} variant="secondary" className="m-2 p-2 " style={{ width: '18rem', borderColor: "#fff", backgroundColor: "#3dcd58", boxShadow: '0 0 30px rgba(0, 0, 0, 0.5)' }}>
+                  <Card.Body>
+                    <Card.Title className="text-light">{project.title}</Card.Title><iframe width="240rem" height="auto" src={project.contentUrl} title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+                    <Card.Text className="text-white">{project.description}
+                    </Card.Text>
+                    <Card.Text>{project.brief2}
+                    </Card.Text>
+                    <Card.Text>{project.brief3}
+                    </Card.Text>
+                    <Button variant="secondary" size="md" className="mb-1 mt-auto" ><a href={project.contentUrlButton} target="_blank" rel="noreferrer" className="btn1" >More information <span><i class="fa fa-external-link" aria-hidden="true"></i></span></a></Button>
+                    {/* <Card.Link href="#">Card Link</Card.Link>
                   <Card.Link href="#">Another Link</Card.Link> */}
-              </Card.Body>
-            </Card>
+                  </Card.Body>
+                </Card>
 
-          )}
-        </div>
-      </Row>
+              )}
+            </div>
+          </Row>
           <Row className="mt-3" >
 
-<Col >
-  <Carousel slide={false}><Carousel.Item>
-    <Image src={process.env.PUBLIC_URL + "/assets/nasa.jpg"} className="img-fluid d-flex flex-wrap justify-content-around about-slide-image" />
-    <Carousel.Caption className='position-relativeslide-title about-outlined-text'>
-      <h5 className='position-relativeslide-title about-outlined-text'>
-        At ASCO Power Technologies, we have a long history of innovation. We are on a constant drive to bring new technologies and new products to our customers to help them become more productive.</h5><br></br>
-    </Carousel.Caption>
-  </Carousel.Item>
-    <Carousel.Item>
-      <Image src={process.env.PUBLIC_URL + "/assets/nyc.jpeg"} className="img-fluid d-flex flex-wrap justify-content-around about-slide-image" />
-      <Carousel.Caption className='position-relativeslide-title about-outlined-cap'>
-        <h5 className='position-relativeslide-title about-outlined-text'>
-          At ASCO, we have a long history of innovation. In fact, it's part of our DNA. Since 1888, ASCO has prided itself on providing cutting edge technology and never compromising on quality.</h5><br></br>
-      </Carousel.Caption>
-    </Carousel.Item>
-  </Carousel>
-  </Col>
+            <Col >
+              <Carousel slide={false}>
+                <Carousel.Item>
+                  <Image src={process.env.PUBLIC_URL + "/assets/nyc.jpeg"} className="img-fluid d-flex flex-wrap justify-content-around about-slide-image-home" />
+                  <Carousel.Caption className='position-relativeslide-title about-outlined-cap'>
+                    <h5 className='position-relativeslide-title about-outlined-text-home'>
+                      Solutions in Critical Power,<br></br>Resolving every challenge,<br></br>
+                      Light in darkest hour.</h5>
+                  </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item>
+                  <Image src={process.env.PUBLIC_URL + "/assets/tokyostreetdigital.jpeg"} className="img-fluid d-flex flex-wrap justify-content-around about-slide-image-home" />
+                  <Carousel.Caption className='position-relativeslide-title about-outlined-cap'>
+                    <h5 className='position-relativeslide-title about-outlined-text-home'>
+                      Reliable and strong,<br></br>
+                      ASCO powers the future,<br></br>
+                      Innovation's song.</h5>
+                  </Carousel.Caption>
+                </Carousel.Item>
+              </Carousel>
+            </Col>
 
 
-</Row>
+          </Row>
         </Container>
       </div>
 
